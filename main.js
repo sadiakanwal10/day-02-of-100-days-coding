8e0084c934852d8430d7a7ae93ed652744a596dd
sadiakanwal10 <sadiakanwal715@gmail.com>
2024-06-27 23:15:07 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+// question no 01 //
+// store an author name in a variable //
+let authorName = "Albert Einstein";
+// print the quote and the name of its author //
+console.log(`${authorName} once said, "A person who never made a mistake never tried anything new"`);
+// question no 02 //
+// store famous person in a variable //
+let famous_person = "QUAID-E-AZAM";
+// store a famous quote and its author name in a variable //
+let message = `${famous_person} once said, "Success consists in moving from failure to failure without loss of enthusiasm"`;
+console.log(message);
+// question no 03 //
+// printing name with whitespace //
+let nameWithWhitespace = "\t \n sadia kanwal \t \n";
+console.log("NAME WITH WHITESPACE:", nameWithWhitespace);
+// printing name after removing whitespace //
+let nameWithoutWhitespace = nameWithWhitespace.trim();
+console.log("NAME WITHOUT WHITESPACE:", nameWithoutWhitespace);
---
+++ main.ts
@@ -0,0 +1,25 @@
+// question no 01 //
+// store an author name in a variable //
+let authorName:string = "Albert Einstein";
+
+// print the quote and the name of its author //
+console.log(`${authorName} once said, "A person who never made a mistake never tried anything new"`);
+
+// question no 02 //
+// store famous person in a variable //
+let famous_person:string="QUAID-E-AZAM";
+
+// store a famous quote and its author name in a variable //
+let message:string = `${famous_person} once said, "Success consists in moving from failure to failure without loss of enthusiasm"`;
+console.log(message);
+
+// question no 03 //
+
+// printing name with whitespace //
+let nameWithWhitespace:string = "\t \n sadia kanwal \t \n";
+console.log("NAME WITH WHITESPACE:", nameWithWhitespace);
+ 
+// printing name after removing whitespace //
+let nameWithoutWhitespace:string= nameWithWhitespace.trim();
+console.log("NAME WITHOUT WHITESPACE:", nameWithoutWhitespace);
+                                                                                                                                                                    
